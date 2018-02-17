0ce17a65b4c7e041e4b38e512d8f845859759a3f
pooya-tolideh <pooya.tolideh@gmail.com>
2018-02-17 18:20:07 -0500

    micro redux-store test was added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+
+import configureStore from './store/configure';
+
+const store = configureStore();
+
+
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/visbleExpenses';
+
+store.subscribe(() => {
+    const state = store.getState();
+    console.table(
+        getVisibleExpenses(state)
+    );
+});
+
+store.dispatch(addExpense({
+    description: 'car wash',
+    amount: 20
+}));
+
+store.dispatch(addExpense({
+    description: 'home repair',
+    amount: 40
+}));
+
+store.dispatch(setTextFilter('car'));
 
 
 ReactDOM.render( <AppRouter/> , document.getElementById('app'));
