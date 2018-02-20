0aa6b0d4dc64b295578fdc5aa39628ce23a2f939
pooya-tolideh <pooya.tolideh@gmail.com>
2018-02-20 00:43:06 -0500

    add new expenses through AddExpenseForm.js component

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default class ExpenseForm extends React.Component {
+    
+    state = {
+        description : '',
+        amount: 0,
+        note: ''
+    }
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({description}));
+    }
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        this.setState(() => ({amount}));
+    }
+
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({note}));
+    }   
+
+    render() {
+        return (
+            <div>
+                <input
+
+                    placeholder="expense description" 
+                    type="text"
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input 
+                    type="number"
+                    placeholder="amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <textarea 
+                    cols="30" rows="10" 
+                    placeholder="Add a note"
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                >
+                </textarea>
+                <button>Add Expense</button>
+
+            </div>
+        );
+    }
+}
+
+// export default connect(, )(ExpenseForm);
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpnseDashboardPage';
-import AddExpensePage from '../components/ExpnseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import PageNotFound from '../components/PageNotFound';
