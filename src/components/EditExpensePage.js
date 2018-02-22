0cd26c322ac1df86af053c2b6225cec09a10e0c8
pooya-tolideh <pooya.tolideh@gmail.com>
2018-02-21 23:23:47 -0500

    reused and mod addExpense comp to create editExpense page for each expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => (
-    <div>
-        { ` Edit expense # ${props.match.params.id } here` }
-    </div>
-);
 
-export default EditExpensePage;
+const EditExpensePage = (props) => {
+    const {id:expenseId} = props.match.params;
+    return (
+        <div>
+            <h1>{ ` Edit expense # ${expenseId}` }</h1>
+            <ExpenseForm 
+                onSubmit={(expense) => {
+                    props.dispatch(editExpense(expenseId, expense));
+                }}
+                push={props.history.push}
+            />
+        </div>
+    );
+};
+
+export default connect()(EditExpensePage);
